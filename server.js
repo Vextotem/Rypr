1e62547686c0a811afebc71032a4784eeec603c1
Vextotem <iftmediainfo@gmail.com>
2024-10-20 09:01:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Allows requests from any origin
 app.use(express.json());
 
 // Import routes
